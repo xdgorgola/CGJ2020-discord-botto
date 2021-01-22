44982044f6a8ae77bef67943b4a32906a1759ce1
xdgorgola <15-11264@usb.ve>
2021-01-21 20:30:24 -0400

    Revert "Merge pull request #1 from aitorres/main"
    This reverts commit d3ea3e33d68007824979e7e6670eb08753d7456b, reversing
    changes made to 5a23b0af6e329f961efdd40cee6afce36c9ede40.

--- events/roleReactEvent.js
+++ events/roleReactEvent.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 module.exports = {
     /**
-     * @param {Discord.Client} client Cliente de Discord del bot
+     * @param {Discord.Client} client Cliente de Discord del bot 
      * @param {object} reactRolesData Data de reacciones
      */
     rolerReactAddEvent(client, reactRolesData)
@@ -11,38 +11,38 @@ module.exports = {
             if (reactRolesData.channelID === undefined || reactRolesData.messageID === undefined ||
                 !re.message.guild || us.bot)
                 return;
-
-            if (reactRolesData.channelID !== re.message.channel.id ||
+        
+            if (reactRolesData.channelID !== re.message.channel.id || 
                 reactRolesData.messageID !== re.message.id)
             {
-                console.log("Ignorar reacción: messageReactionAdd");
+                console.log("Ignorar reaccion");
                 return;
             }
 
-            try
+            try 
             {
                 const validReactions = reactRolesData.reactionMap;
                 var roleIDToAdd = null;
-
+        
                 if (re.emoji.id != null)
-                    roleIDToAdd = validReactions[re.emoji.id];
+                    roleIDToAdd = validReactions.get(re.emoji.id);
                 else
-                    roleIDToAdd = validReactions[re.emoji.toString()];
-
+                    roleIDToAdd = validReactions.get(re.emoji.toString());
+        
                 var role = re.message.guild.roles.cache.get(roleIDToAdd);
-
+        
                 //console.log(re.emoji.toString());
                 //await re.message.channel.send("Emoji string: " + re.emoji.toString());
                 //await re.message.channel.send("Reaction emoji ID: " + re.emoji.id);
-
+        
                 if (!role) {
                     console.log("Wrong emoji!");
                     await re.remove()
                     return;
-                }
-
+                }   
+        
                 await re.message.guild.member(us).roles.add(role);
-
+        
                 console.log(reactRolesData.channelID);
                 console.log(reactRolesData.messageID);
             }
@@ -56,7 +56,7 @@ module.exports = {
         });
     },
     /**
-     * @param {Discord.Client} client Cliente de Discord del bot
+     * @param {Discord.Client} client Cliente de Discord del bot 
      * @param {object} reactRolesData Data de reacciones
      */
     roleReactRemoveEvent(client, reactRolesData)
@@ -65,28 +65,28 @@ module.exports = {
             if (!reactRolesData.channelID || !reactRolesData.messageID ||
                 !re.message.guild || us.bot)
                 return;
-
-            if (reactRolesData.channelID !== re.message.channel.id ||
+            
+            if (reactRolesData.channelID !== re.message.channel.id || 
                 reactRolesData.messageID !== re.message.id)
             {
-                console.log("Ignorar reaccion: messageReactionRemove");
+                console.log("Ignorar reaccion");
                 return;
             }
 
             const validReactions = reactRolesData.reactionMap;
             var roleToRemove = null;
-
+        
             if (re.emoji.id != null)
-                roleToRemove = validReactions[re.emoji.id];
+                roleToRemove = validReactions.get(re.emoji.id);
             else
-                roleToRemove = validReactions[re.emoji.toString()];
-
+                roleToRemove = validReactions.get(re.emoji.toString());
+        
             console.log(re.emoji.toString());
             //await re.message.channel.send("Emoji string: " + re.emoji.toString());
             //await re.message.channel.send("Reaction emoji ID: " + re.emoji.id);
-
+        
             await re.message.guild.member(us).roles.remove(roleToRemove);
-
+        
             console.log(reactRolesData.channelID);
             console.log(reactRolesData.messageID);
         });

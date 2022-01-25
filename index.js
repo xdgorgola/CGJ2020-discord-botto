ce7db13a665ca98a674acd5f61c74d689027520a
Andrés Ignacio Torres <andresitorresm@gmail.com>
2022-01-24 23:31:14 -0400

    fix: add necessary intents to Discord client

--- index.js
+++ index.js
@@ -17,10 +17,17 @@ const Intents = Discord.Intents;
 const client = new Discord.Client({
   partials: ["MESSAGE", "GUILD_MEMBER", "REACTION", "USER"],
   intents: [
+    Intents.FLAGS.GUILDS,
     Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_BANS,
+    Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+    Intents.FLAGS.GUILD_PRESENCES,
     Intents.FLAGS.GUILD_MESSAGES,
     Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.GUILD_MESSAGE_TYPING,
     Intents.FLAGS.DIRECT_MESSAGES,
+    Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+    Intents.FLAGS.DIRECT_MESSAGE_TYPING,
   ],
   ws: { intents: Discord.Intents.ALL },
 });
